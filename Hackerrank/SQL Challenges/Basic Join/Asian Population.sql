832fa5bfb75eb9247b1ddb9c5f4a989f96868ae7
Barath Gopinath <barathgopi1699@gmail.com>
2020-09-13 12:39:39 +0530

    join

---
+++ Hackerrank/SQL Challenges/Basic Join/Asian Population.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/asian-population/problem
+
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY 
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = "Asia";
