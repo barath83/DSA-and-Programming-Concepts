d446e69361f107a4641491c3a965da7cc485f31c
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-11 12:29:42 +0530

    quick sort

--- Algorithms/Sorting/Quick Sort.cpp
+++ Algorithms/Sorting/Quick Sort.cpp
@@ -11,18 +11,28 @@ void swap(int *a,int *b)
 
 int partition(int a[],int lo,int hi)
 {
+    //taking pivot as the hi pointer element
     int pivot = a[hi];
+    //intiliazing i to -1 
     int i = lo-1;
 
+    //iteration from lo pointer to an element before than hi pointer
     for(int j=lo;j<=hi-1;j++){
+        //if the current element is lesser than pivot
         if(a[j]<pivot)
         {
+            //increment the value of i 
             i++;
+            //swap the element at curent index with the element at ith index
             swap(&a[i],&a[j]);
         }
-        
     }
+    //after complete iteration for this function call 
+    //swap the i+1th element with the pivot element
+    //pivot now occupies it's exact sorted position i.e elements to left of pivot are smaller than pivot
+    //elements to right of pivot are larger than pivot
     swap(&a[i+1],&a[hi]);
+    //return i+1 as it is the new position of pivot 
     return i+1;
 }
 
@@ -30,10 +40,14 @@ int partition(int a[],int lo,int hi)
 
 void quickSort(int a[],int lo,int hi)
 {
+    //if lo pointer is lesser than high pointer proceed
     if(lo<hi){
+        //identify the pivot's new position by calling partition function
         int piv = partition(a,lo,hi);
 
+        //call quickSort on the left side elements from pivot's position
         quickSort(a,lo,piv-1);
+        //call quickSort on the right side elements from pivot's position
         quickSort(a,piv+1,hi);
     }
 }
