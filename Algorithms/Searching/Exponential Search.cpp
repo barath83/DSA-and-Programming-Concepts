4a9456b97c10bf9b393a93d679d00a2b0eaf4c8f
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-11 22:52:32 +0530

    exponential search

---
+++ Algorithms/Searching/Exponential Search.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int a[],int l,int r,int k)
+{
+    if(r>=l)
+    {
+        int mid = l+(r-l)/2;
+
+        //if k is the mid index element return it
+        if(a[mid]==k)
+            return mid;
+        //if mid index element lesser than k return the right subarray for binary search    
+        //else left subarray
+        if(a[mid]<k)
+            return binarySearch(a,mid+1,r,k);
+        else
+            return binarySearch(a,l,mid-1,k);        
+    }
+
+}
+
+
+int expSearch(int a[],int n,int k)
+{
+    //if the element at 0 is the required k return 0
+    if(a[0]==k)
+        return 0;
+
+    int i = 1 ;
+    while(i<n&&a[i]<=k){
+        i = i*2;
+    }    
+
+    return binarySearch(a,i/2,min(i,n),k);
+}
+
+
+int main()
+{
+    //Accepting the n value for size of array
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+
+    int a[n];
+
+    cout<<"Enter the elements in sorted order"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int k;
+    cout<<"Enter the element to be searched"<<endl;
+    cin>>k;
+
+    int result = expSearch(a,n,k);
+
+    //ELEMENT FOUND
+    if(result!=-1){
+        cout<<"The element is found at position: "<<result+1<<endl;
+    }
+    //ELEMENT NOT FOUND
+    else{
+        cout<<"Element is not present in the array"<<endl;
+    }
+}
