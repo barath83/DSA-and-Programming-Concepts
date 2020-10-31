443b773578643954457e4e0a90d32ed6ae64bac5
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-31 14:59:10 +0530

    polymorhphism

---
+++ OOPS/Polymorphism/polymorphism.cpp
@@ -0,0 +1,11 @@
+/*
+ Polymorphism - means many forms 
+ It simply means the ability of a object,function or a variable to take more than one form
+ Like in a real word a person can different characteristics at the same time 
+ a woman can be a mother,daughter,sister,wife all at same time with different perspectives
+
+ Polymorphism is divided into two
+ - Compile time polymorphism - Function Overlaoding,Operator overloading
+ - Run time polymorphism - Virtual Functions-Function overriding
+
+*/
